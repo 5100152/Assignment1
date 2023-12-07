1404189d40ae45e8c857855852ec630296804f4d
5100152 <5100152@mypillar7uwc.ac.za>
2023-12-07 12:41:14 +0200

    Add project files.

---
+++ assignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace assignment1;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int beer = 100;
+        while (beer > 2)
+        {
+            Console.WriteLine(beer + " bottles of beer on the wall," + beer + " bottles of beer");
+            beer--;
+            Console.WriteLine("Take one down and pass it around, " + beer + " bottles of beer on the wall.");
+        }
+        Console.WriteLine(beer + " bottles of beer on the wall," + beer + " bottles of beer");
+        beer--;
+        Console.WriteLine("Take one down and pass it around, " + beer + " bottle of beer on the wall.");
+        Console.WriteLine(beer + " bottle of beer on the wall," + beer + " bottle of beer");
+        
+    }
+}
